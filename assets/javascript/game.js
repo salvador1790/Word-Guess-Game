2ff71712560b78ed66e74e305567f9235d06f962
Salvador Murillo <chavitation@gmail.com>
2019-06-09 20:24:28 -0700

    first commit

---
+++ assets/javascript/game.js
@@ -0,0 +1,54 @@
+document.onkeyup = function gamestart() {
+    // array of words for the game
+    var wordBank = ["ghost", "goblin", "witch"];
+
+    //ramdom word picker-will only select one.
+    var randWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+
+
+    // The array where the correct answers will be pushed to
+    var answerArr = [];
+
+    var guessedLetters = [];
+
+    var guessesRemaining = 12;
+
+    var wins = 0;
+
+
+
+    for (var i = 0; i < randWord.length; i++) {
+        answerArr.push('_');
+        document.getElementById("word-div").textContent = answerArr.join(' ')
+    }
+
+
+    //what will keep track of how many more letters we have left to guess
+    var leftToGuess = randWord.length;
+    
+
+
+    // this function grabs the user guess and compares to the random word generated and displays the letter if correct.
+    document.onkeyup = function () {
+        var currentClickGuess = event.key;
+        for (var i = 0; i < leftToGuess; i++) {
+            if (currentClickGuess === randWord[i]) {
+                answerArr[i] = currentClickGuess;
+                document.getElementById("word-div").innerHTML = answerArr.join(' ');
+                audio.play();
+                document.getElementById("guessed-letters").innerHTML= currentClickGuess;
+            } 
+        
+        }
+    
+    }
+
+};
+
+
+
+
+
+
+
+
